fe59d541ca25039047e280dbc9993baacb03f6d4
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2019-11-20 17:03:31 +0100

    creat minimal function image

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: roalvare <roalvare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/16 19:50:29 by roalvare          #+#    #+#             */
-/*   Updated: 2019/11/20 14:42:02 by roalvare         ###   ########.fr       */
+/*   Updated: 2019/11/20 17:01:28 by roalvare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,13 @@ typedef struct s_windows
 	int			height;
 }				t_windows;
 
+typedef struct s_img
+{
+	void		*id;
+	int			width;
+	int			height;
+}				t_img;
+
 typedef struct s_rgb
 {
 	double		red;
@@ -39,9 +46,12 @@ void			*free_windows(t_windows	*windows);
 
 int				key_hook(int keycode, void *param);
 
+void			img_pixel_put(t_img *img, int x, int y, unsigned int color);
+void 			print_colunm(t_img *windows, void *mlx, int colunm, int color);
+void			print_color(t_img *windows, void *mlx, int color);
+
 void	print_line(t_windows *windows, void *img, int line, int color);
-void 	print_colunm(t_windows *windows, int colunm, int color);
-void	print_color(t_windows *windows, int color);
+
 void	print_gradiant(t_windows *windows, int color1, int color2, int begin, int end);
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: roalvare <roalvare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/16 21:53:39 by roalvare          #+#    #+#             */
-/*   Updated: 2019/11/20 14:41:34 by roalvare         ###   ########.fr       */
+/*   Updated: 2019/11/20 17:01:56 by roalvare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ int		main(int argc, char *argv[])
 		return (EXIT_FAILURE);
 	if (!(windows = create_windows(link_id, 1080, 720, "cub3d")))
 		return (EXIT_FAILURE);
+	// print_color(windows, 0x45E7ad);
 	mlx_key_hook(windows->id, &key_hook, windows);
 	mlx_loop(link_id);
 	free_windows(windows);
@@ -45,6 +46,19 @@ t_windows	*create_windows(void *link_id, int width, int height, char *title)
 	windows->height = height;
 	windows->link_id = link_id;
 	return (windows);
+}
+
+t_img	*create_img(void *mlx, int width, int height)
+{
+	t_img	*img;
+	
+	if (!(img = malloc(sizeof(t_img))))
+		return (NULL);
+	if (!(img->id = mlx_new_image(mlx, width, height)))
+		return (NULL); //
+	img->width = width;
+	img->height = height;
+	return (img);
 }
 
 void	*free_windows(t_windows	*windows)
@@ -66,12 +80,38 @@ int	key_hook(int keycode, void *param)
 	}
 	else if (keycode == 0)
 	{
-		void *img = mlx_new_image(windows->link_id, windows->width, windows->height);
-		// print_line(windows, img, 200)
+		t_img *img = create_img(windows->link_id, windows->width, windows->height);
+		print_color(img, windows->link_id, 0x45E7ad);
 		// print_color(windows, 0x45E7ad);
-		mlx_put_image_to_window(windows->link_id, windows->id, img, 0, 0);
+		mlx_put_image_to_window(windows->link_id, windows->id, img->id, 0, 0);
 	}
 	return (EXIT_SUCCESS);
+}
+
+void	img_pixel_put(t_img *img, int x, int y, unsigned int color)
+{
+	char	*begin;
+	int		bpp;
+	int		size_l;
+	int		endian;
+
+	begin = mlx_get_data_addr(img->id, &bpp, &size_l, &endian);
+	begin += (y * size_l);
+	begin += (x * bpp / (sizeof(char) * 8));  
+	*begin = color;
+	*(begin + 1) = color / 256 ;
+	*(begin + 2) = color / 256 / 256;
+}
+
+void 	print_colunm(t_img *img, void *mlx, int colunm, int color)
+{
+	int 			i;
+	unsigned int	u_color;
+
+	u_color = mlx_get_color_value(mlx, color);
+	i = -1;
+	while (++i < img->height)
+		img_pixel_put(img, colunm, i, u_color);
 }
 
 void	print_line(t_windows *windows, void *img, int line, int color)
@@ -83,20 +123,11 @@ void	print_line(t_windows *windows, void *img, int line, int color)
 		mlx_pixel_put(windows->link_id, img, i, line, color);
 }
 
-void 	print_colunm(t_windows *windows, int colunm, int color)
-{
-	int i;
-
-	i = -1;
-	while (++i < windows->height)
-		mlx_pixel_put(windows->link_id, windows->id, colunm, i, color);
-}
-
-void	print_color(t_windows *windows, int color)
+void	print_color(t_img *img, void *mlx, int color)
 {
 	int x;
 
 	x = -1;
-	while (++x < windows->width)
-		print_colunm(windows, x, color);
+	while (++x < img->width)
+		print_colunm(img, mlx , x, color);
 }
