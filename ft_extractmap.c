e4f2936507abf0e4b6ab8d05f6d2b75d0b917cba
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2019-11-23 20:58:34 +0100

    set position

--- ft_extractmap.c
+++ ft_extractmap.c
@@ -6,7 +6,7 @@
 /*   By: roalvare <roalvare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/23 12:03:06 by roalvare          #+#    #+#             */
-/*   Updated: 2019/11/23 18:47:13 by roalvare         ###   ########.fr       */
+/*   Updated: 2019/11/23 20:57:34 by roalvare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,30 +88,78 @@ char	*extract_line_map(char *line, char ***map)
 	return (NULL);
 }
 
-char	*analize_map(char **map)
+char	isdir(char c)
+{
+	if (c == 'N')
+		return ('N');
+	else if (c == 'S')
+		return ('S');
+	else if (c == 'W')
+		return ('W');
+	else if (c == 'E')
+		return ('E');
+	return (0);
+}
+
+void	set_player(t_player *ply, int x, int y, char dir)
+{
+	ply->x = (double)x + 0.5;
+	ply->y = (double)y + 0.5;
+	if (dir == 'N')
+	{
+		ply->dirx = ply->x;
+		ply->diry = y;
+	}
+	else if (dir == 'S')
+	{
+		ply->dirx = ply->x;
+		ply->diry = y + 1;
+	}
+	else if (dir == 'E')
+	{
+		ply->dirx = x;
+		ply->diry = ply->y;
+	}
+	else if (dir == 'W')
+	{
+		ply->dirx = x + 1;
+		ply->diry = ply->y;
+	}
+}
+
+char	*analize_map(char **map, t_player *ply)
 {
 	size_t	len;
-	char	*cursor;
 	int		lenm;
+	int		x;
+	int		y;
 
+	y = 0;
 	len = ft_strlen(*map);
 	lenm = ft_tablen(map);
 	printf("=============\n");
-	while (*map)
+	while (map[y])
 	{
-		cursor = *map;
-		printf("[MAP] = %s\t", *map);
-		if (len != ft_strlen(*map))
+		x = 0;
+		printf("[MAP] = %s\t[y] = %d\n", map[y], y);
+		if (len != ft_strlen(map[y]))
 			return ("Wrong lenght line in map");
-		if (*cursor != '1' || cursor[ft_strlen(cursor) - 1] != '1')
+		if (map[y][x] != '1' || map[y][ft_strlen(map[y]) - 1] != '1')
 			return ("Border map error");
-		while (*cursor)
+		while (map[y][x])
 		{
-			if ((ft_tablen(map) == lenm || ft_tablen(map) == 1) && *cursor != '1')
+			if ((y == ft_tablen(map) || y == 0) && map[y][x] != '1')
 				return ("Border map error");
-			cursor++;
+			if (isdir(map[y][x]) && (ply->x != 0))
+			{
+				printf("[ply] = %f\n", ply->x);
+				return ("Two player in map");
+			}
+			else if (isdir(map[y][x]))
+				set_player(ply, x, y, isdir(map[y][x]));
+			x++;
 		}
-		map++;
+		y++;
 	}
 	return (NULL);
 }
@@ -128,7 +176,7 @@ char	*extract_map(int fd, char *line, t_game *game)
 	while (get_next_line(fd, &line) >= 0 && ft_strncmp(line, "", 1))
 		if ((error = extract_line_map(line, &game->map.map)))
 			return (error);
-	if ((error = analize_map(game->map.map)))
+	if ((error = analize_map(game->map.map, &game->ply)))
 		return (error);
 	return (NULL);
 }
